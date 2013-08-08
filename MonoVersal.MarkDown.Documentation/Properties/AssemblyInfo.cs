ee685dd2683e9d6921b28daa7b2ceb5d5f804dee
moljac <mcvjetko@holisticware.net>
2013-08-08 14:54:27 +0200

    Initial sample XA

--- MonoVersal.MarkDown.Documentation/Properties/AssemblyInfo.cs
+++ MonoVersal.MarkDown.Documentation/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MonoVersal.MarkDown.Documentation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HolisticWare")]
 [assembly: AssemblyProduct("MonoVersal.MarkDown.Documentation")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ MonoVersal.MarkDown
 ===================
 
 Cross platform Markdown utilities
+
+This is set of 
+
+Markdown.Sharp
+	http://code.google.com/p/markdownsharp/
+	license: MIT licese http://opensource.org/licenses/mit-license.php
+	
