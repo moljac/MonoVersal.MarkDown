529cf417125ec2352a0d50d785f95c561de96cc5
moljac <mcvjetko@holisticware.net>
2014-09-28 20:15:21 +0200

    MarkDownSharp component packaging scripts added investigation of the issue: native ios linking failed...

--- XamarinComponent.MarkDownSharp/component-create.sh
+++ XamarinComponent.MarkDownSharp/component-create.sh
@@ -1,8 +1,8 @@
-@echo off
+#!/bin/bash
 
-set MONO="%PROGRAMFILES%\Mono-3.0.9\bin\mono.exe"
-set XPKG=sbin\xpkg\xpkg.exe
-set RAKE=C:\bin\Ruby200\bin\rake
+MONO=mono
+XPKG=sbin/xpkg/xpkg.exe
+RAKE=rake
 
 
 :: rake downloads xpkg and runs:
@@ -10,22 +10,20 @@ set RAKE=C:\bin\Ruby200\bin\rake
 set PACKAGE=HolisticWare-SharpSerializer
 cd SharpSerializer
 
-%MONO% ^
-	%XPKG% ^
-	create %PACKAGE%-1.0.xam ^
-	--name="HolisticWare SharpSerializer component" ^
-	--summary="%PACKAGE% ported and packaged by HolisticWare" ^
-	--publisher="HolisticWare LLC" ^
-	--website="http://holisticware.net/" ^
-	--details="content/Details.md" ^
-	--license="content/License.md" ^
-	--getting-started="content/GettingStarted.md" ^
-	--icon="content/icons/%PACKAGE%_128x128.png" ^
-	--icon="content/icons/%PACKAGE%_512x512.png" ^
-	--library="ios":"content/bin/%PACKAGE%.iOS.dll" ^
-	--library="android":"content/bin/%PACKAGE%.Android.dll" ^
-	--sample="iOS Sample. Demonstrates Awesomeness on iOS.":"content/samples/Awesome.iOS.sln" ^
+%MONO% \
+	%XPKG% \
+	create %PACKAGE%-1.0.xam \
+	--name="HolisticWare SharpSerializer component" \
+	--summary="%PACKAGE% ported and packaged by HolisticWare" \
+	--publisher="HolisticWare LLC" \
+	--website="http://holisticware.net/" \
+	--details="content/Details.md" \
+	--license="content/License.md" \
+	--getting-started="content/GettingStarted.md" \
+	--icon="content/icons/%PACKAGE%_128x128.png" \
+	--icon="content/icons/%PACKAGE%_512x512.png" \
+	--library="ios":"content/bin/%PACKAGE%.iOS.dll" \
+	--library="android":"content/bin/%PACKAGE%.Android.dll" \
+	--sample="iOS Sample. Demonstrates Awesomeness on iOS.":"content/samples/Awesome.iOS.sln" \
 	--sample="Android Sample. Demonstrates Awesomeness on Android":"content/samples/Awesome.Android.sln"
 
-	
-pause
